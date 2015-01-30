61a7a662a83913cabc165fe9f763008ac950eaae
m6urns <m6urns@gmail.com>
2015-01-30 21:28:17 +0000

    Reworked switch.py as support for pikeyd

--- switch.py
+++ switch.py
@@ -1,21 +1,20 @@
 import RPi.GPIO as GPIO
-import pexpect
+#import pexpect
 import time
+import os
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#while True:
-#    input_state = GPIO.input(18)
-#    if input_state == False:
-#        print('Button Pressed')
-#        time.sleep(0.2)
-
-while False:
+while True:
     input_state = GPIO.input(18)
-    if input_state == True:
-        print('Its might work?')
-        print('\x1b')
+    if input_state == False:
+        print('False')
         time.sleep(0.2)
+    else: 
+	if input_state == True:
+           os.spawnl(os.P_NOWAIT, 'pikeyd restart')
+           time.sleep(0.2)
+            
 
